b6f315f0da6d5397b8225ecaa31c9ba56ea3f490
leo <krokxa228@gmail.com>
2024-08-06 15:04:07 +0500

    Inition commit

---
+++ csv_merge.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# Загрузка данных из первого файла
+file1 = 'data/backup/ds_21.xlsx'
+df1 = pd.read_excel(file1)
+
+# Загрузка данных из второго файла
+file2 = 'data/backup/ds_22.xlsx'
+df2 = pd.read_excel(file2)
+
+file3 = 'data/backup/ds_23.xlsx'
+df3 = pd.read_excel(file3)
+
+file4 = 'data/backup/ds_24.xlsx'
+df4 = pd.read_excel(file4)
+
+# Объединение данных
+combined_df = pd.concat([df1, df2])
+
+# Сохранение объединенных данных в новый файл
+output_file = 'combined_file.xlsx'
+combined_df.to_excel(output_file, index=False)
+
+print(f"Данные успешно объединены и сохранены в {output_file}")
