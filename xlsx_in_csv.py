4e7874ec63117fa2da1e0a85d401bad1f3f7f9af
LEO <krokxa228@gmail.com>
2024-08-08 17:16:20 +0500

    added more params and grafs

--- xlsx_in_csv.py
+++ xlsx_in_csv.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # of an excel file  
-read_file = pd.read_excel ("MyDataset.xlsx") 
+read_file = pd.read_excel ("MyDataset_12_twice.xlsx") 
   
 # Write the dataframe object 
 # into csv file 
-read_file.to_csv ("MyDataset.csv",  
+read_file.to_csv ("MyDataset_12_twice.csv",  
                   index = None, 
                   header=True) 
     
